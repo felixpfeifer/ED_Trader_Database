58557ef6b1c404bd41160f060317130bf7d0900c
Felix Pfeifer <felixpfeifer@t-online.de>
2023-07-22 23:45:33 +0200

    Projektdateien hinzufügen.

---
+++ ED_Trader_Database_Part/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED_Trader_Database
+{
+    public  class Program
+    {
+        public static void Main(string[] args) 
+        {
+
+        }
+    }
+}
